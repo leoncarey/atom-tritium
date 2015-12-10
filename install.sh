41839f257b8cdbc6df8b3b74e6e69ea7564bd43c
Leonardo Carey <leonardo.carey@objectedge.com>
2015-12-10 16:04:22 -0200

    README has been updated

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-mkdir ~/.atom/packages/atom-tritium-snippets
-cp -r atom-tritium-master/* ~/.atom/packages/atom-tritium-snippets/
+mkdir ~/.atom/packages/atom-tritium
+cp -r atom-tritium-master/* ~/.atom/packages/atom-tritium/
 cat atom-tritium-master/styles.less >> ~/.atom/styles.less
 rm -R atom-tritium-master
+printf "\n\n=================================== Congratulations! ====================================\n================== Now you can build your Tritium projects using Atom! ==================\n\n"
